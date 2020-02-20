ff507bc8e5ba0f42474eddf2fd317c74b3c17475
Dzmitry Hlushak <hlushak.dzmitry@gmail.com>
2020-02-20 23:15:02 +0300

    feat: add task typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined) return 0;
+  if(array.length == 0) return 0;
+
+  return array.sort((a,b)=> {return a-b})[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined) return 0;
+  if(array.length == 0) return 0;
+  
+  return array.sort((a,b)=> {return b-a})[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined) return 0;
+  if(array.length == 0) return 0;
+
+  return array.reduce((total, elem)=> total +=elem,0) / array.length;
 }
